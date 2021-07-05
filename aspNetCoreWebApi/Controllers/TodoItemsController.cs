ae6217fa6aaaf9888307ba6b4adbc1828e6ffa3a
hoang011220 <71324252+hoang011220@users.noreply.github.com>
2021-07-05 17:17:19 +0700

    Update TodoItemsController.cs

--- aspNetCoreWebApi/Controllers/TodoItemsController.cs
+++ aspNetCoreWebApi/Controllers/TodoItemsController.cs
@@ -9,6 +9,7 @@ using aspNetCoreWebApi.Models;
 
 namespace aspNetCoreWebApi.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class TodoItemsController : ControllerBase
@@ -20,15 +21,26 @@ namespace aspNetCoreWebApi.Controllers
             _context = context;
         }
 
+
+        /// <summary>
+        /// Get TodoItems.
+        /// </summary>
         // GET: api/TodoItems
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
         {
             return await _context.TodoItems.ToListAsync();
         }
 
+        /// <summary>
+        /// Get a TodoItem.
+        /// </summary>
         // GET: api/TodoItems/5
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
         {
             var todoItem = await _context.TodoItems.FindAsync(id);
@@ -40,7 +52,9 @@ namespace aspNetCoreWebApi.Controllers
 
             return todoItem;
         }
-
+        /// <summary>
+        /// Change info a TodoItem.
+        /// </summary>
         // PUT: api/TodoItems/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
@@ -72,9 +86,31 @@ namespace aspNetCoreWebApi.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Creates a TodoItem.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///         "id": 0,
+        ///         "name": "string",
+        ///         "isComplete": true,
+        ///         "secret": "string",
+        ///         "age": 0
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="todoItem"></param>
+        /// <returns>A newly created TodoItem</returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>  
         // POST: api/TodoItems
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
         {
             _context.TodoItems.Add(todoItem);
@@ -84,6 +120,11 @@ namespace aspNetCoreWebApi.Controllers
             return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
         }
 
+
+        /// <summary>
+        /// Deletes a specific TodoItem.
+        /// </summary>
+        /// <param name="id"></param>
         // DELETE: api/TodoItems/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteTodoItem(long id)
