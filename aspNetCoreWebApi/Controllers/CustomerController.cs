88f28b5e2dac499f9ccd19ad8ca026343c2202fc
hoang011220 <71324252+hoang011220@users.noreply.github.com>
2021-07-07 20:30:39 +0700

    Create CustomerController.cs

---
+++ aspNetCoreWebApi/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+
+namespace aspNetCoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpPatch]
+        public IActionResult JsonPatchForDynamic([FromBody] JsonPatchDocument patch)
+        {
+            dynamic obj = new ExpandoObject();
+            patch.ApplyTo(obj);
+
+            return Ok(obj);
+        }
+    }
+}
