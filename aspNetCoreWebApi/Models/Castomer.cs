a8afe1d8465cb775a84b4595cfe0b223c813c02f
hoang011220 <71324252+hoang011220@users.noreply.github.com>
2021-07-07 20:29:55 +0700

    Create Castomer.cs

---
+++ aspNetCoreWebApi/Models/Castomer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspNetCoreWebApi.Customer
+{
+    public class Castomer
+    {
+        public string CustomerName { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
