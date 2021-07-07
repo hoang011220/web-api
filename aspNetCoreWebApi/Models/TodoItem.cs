4eac848f4e863b35ac6dfa64c9736d4af12d0652
hoang011220 <71324252+hoang011220@users.noreply.github.com>
2021-07-07 20:29:40 +0700

    Update TodoItem.cs

--- aspNetCoreWebApi/Models/TodoItem.cs
+++ aspNetCoreWebApi/Models/TodoItem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,16 @@ namespace aspNetCoreWebApi.Models
 {
     public class TodoItem
     {
+        [Key]
         public long Id { get; set; }
-        [Required]
+
+        [Column(TypeName = "nvarchar(100)")]
         public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
         public bool IsComplete { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
         public string Secret { get; set; }
-        public int Age { get; set; }
     }
 }
