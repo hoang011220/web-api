7523e76d996aa7b94ef09fd0a27aceefc8d63679
hoang011220 <71324252+hoang011220@users.noreply.github.com>
2021-07-07 20:30:02 +0700

    Create 20210707020541_InitialCreate.cs

---
+++ aspNetCoreWebApi/Migrations/20210707020541_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace aspNetCoreWebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    IsComplete = table.Column<string>(type: "nvarchar(5)", nullable: false),
+                    Secret = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Age = table.Column<string>(type: "nvarchar(3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+        }
+    }
+}
