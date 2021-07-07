39a46cafa19c61c0124fd410d86fb1e70324ad3b
hoang011220 <71324252+hoang011220@users.noreply.github.com>
2021-07-07 20:29:37 +0700

    Create ProductContext.cs

---
+++ aspNetCoreWebApi/Models/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspNetCoreWebApi.Models1
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
