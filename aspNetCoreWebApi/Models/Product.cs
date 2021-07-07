90bdb321db65087b9bd69ad46cba73081144dbac
hoang011220 <71324252+hoang011220@users.noreply.github.com>
2021-07-07 20:29:45 +0700

    Create Product.cs

---
+++ aspNetCoreWebApi/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspNetCoreWebApi.Models1
+{
+    public class Product
+    {
+      
+        public long Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+        public bool IsOnSale { get; internal set; }
+    }
+}
